a83fa3a55a3650ba6a3d236a658df00ebd2d85e7
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-04 00:45:14 +0800

    added axios to globalthis

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,8 +1,11 @@
 /* eslint-disable no-var */
+import { AxiosInstance } from "axios";
 import { Connection } from "mysql";
 
 declare var db: Connection;
+declare var axios: AxiosInstance;
 
 declare global {
     var db: Connection;
+    var axios: AxiosInstance;
 }
