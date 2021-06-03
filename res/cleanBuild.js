4819c7beeff2c5dd1b4337cc580e88827c3038b2
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-03 21:39:05 +0800

    added npm run start to compile

--- res/cleanBuild.js
+++ res/cleanBuild.js
@@ -1,2 +1,5 @@
-import fs from "fs";
-fs.rmdirSync("../build", { recursive: true, force: true });
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+require("fs").rmSync("../build", { recursive: true, force: true });
+require("child_process").execSync(`cd ${__dirname}\n./build.sh\n`);
