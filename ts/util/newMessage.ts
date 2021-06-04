644299e00a27cf3de5a721cbba47351c2b1a8077
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-05 00:37:49 +0800

    added query string

--- ts/util/newMessage.ts
+++ ts/util/newMessage.ts
@@ -1,9 +1,18 @@
+import { Message } from "discord.js";
 import { Connection } from "mysql";
 
 const db: Connection = globalThis.db;
 
-export default function(): void {
-    db.query({
-        
-    })
+export default function(message: Message): void {
+    db.query(
+        `INSERT INTO origin_message (messageid, server_origin, author)
+        VALUES (
+            ${message.id},
+            (SELECT localid
+             FROM servers
+             WHERE serverid=${message.guild.id}),
+            (SELECT localid
+             FROM users
+             WHERE localid=${message.author.id})
+        )`); // TODO make check that server and user exists in their respective table
 }
