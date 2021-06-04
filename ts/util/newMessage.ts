71faa374adef6459e562b420f1e2a30ee65851b0
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-05 03:39:32 +0800

    added user check

--- ts/util/newMessage.ts
+++ ts/util/newMessage.ts
@@ -3,16 +3,33 @@ import { Connection } from "mysql";
 
 const db: Connection = globalThis.db;
 
+const cache: string[] = [];
+
 export default function(message: Message): void {
+    if (!cache.indexOf(message.author.id)) {
+        db.query(`
+        SELECT userid
+        FROM users
+        WHERE userid=${parseInt(message.author.id)};`, (error, results, fields) => {
+            if (error) {
+                return;
+            }
+            if (results.length != 1) {
+                return;
+            }
+            cache.push(message.author.id);
+        });
+    }
+
     db.query(
         `INSERT INTO origin_message (messageid, server_origin, author)
         VALUES (
             ${message.id},
             (SELECT localid
              FROM servers
-             WHERE serverid=${message.guild.id}),
+             WHERE serverid=${parseInt(message.guild.id)}),
             (SELECT localid
              FROM users
-             WHERE localid=${message.author.id})
-        )`); // TODO make check that server and user exists in their respective table
+             WHERE localid=${parseInt(message.author.id)})
+        )`);
 }
