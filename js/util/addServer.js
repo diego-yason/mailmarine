dbb865b1a9fd6f7ab911b72cbdbfd93a8ca2ef9c
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-05 17:46:42 +0800

    deleting all files and updated settings

--- js/util/addServer.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const db = globalThis.db;
-function default_1(serverId) {
-    db.query(`INSERT INTO servers (serverid)
-        VALUES (${serverId});`);
-}
-exports.default = default_1;
-//# sourceMappingURL=addServer.js.map
