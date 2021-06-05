b499390733dc8abd69806b725a96f7d139d8ebb9
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-05 13:34:56 +0800

    changed type from commonjs to module

--- res/cleanBuild.js
+++ res/cleanBuild.js
@@ -1,6 +1,10 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-require("fs").rmSync("../build", { recursive: true, force: true });
-require("child_process").execSync(`cd ${__dirname}\n./build.sh\n`);
-require("../js/index.js")();
+import fs from "fs";
+import child_process from "child_process";
+import bot from "../js/index.js";
+
+fs.rmSync("../build", { recursive: true, force: true });
+child_process.execSync(`cd ${__dirname}\n./build.sh\n`);
+bot();
