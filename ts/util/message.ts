b563915ea5c5791b8b9d3ee4ea37812a007873ae
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-08 03:01:18 +0800

    id fields are now varchars

---
+++ ts/util/message.ts
@@ -0,0 +1,20 @@
+import { Message } from "discord.js";
+
+const db = globalThis.db;
+const readfile = globalThis.readSql;
+
+export const createMessage = async (message: Message): Promise<void> => {
+    // check if user exists in the db and isn't banned
+    const readUser = await db.execute(readfile("readUsersById.sql"), [message.author.id]);
+
+    if (!readUser[0][0].id) {
+        // user isn't registered yet to the system
+        await db.query(readfile("newUser.sql"), [message.author.id]);
+    }
+
+    db.execute(readfile("newMessage.sql"), [message.id, message.guild.id, message.author.id]);
+};
+
+export const deleteMessage = async (messageId) => {
+
+};
