41c9183a9c79d49dd64cb94d2e4738bf85456e92
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-07 23:24:53 +0800

    added readsql var

--- types/global.d.ts
+++ types/global.d.ts
@@ -6,9 +6,11 @@ import { AxiosRequestConfig } from "axios";
 import { Pool } from "mysql2/promise";
 
 declare global {
-    var db: Pool;
-    var axios : AxiosRequestConfig;
+    var db:      Pool;
+    var axios:   AxiosRequestConfig;
+    var readSql: (file: string) => string;
 }
 
-declare var db: Pool;
-declare var axios : AxiosRequestConfig;
+declare var db:      Pool;
+declare var axios :  AxiosRequestConfig;
+declare var readSql: (file: string) => string;
