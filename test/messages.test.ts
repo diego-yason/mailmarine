d7db0afb8f6227ec2ee41e26a2277f7035136dcb
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-07 21:30:35 +0800

    tsconfig isn't needed for testing

---
+++ test/messages.test.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const mysql = require("mysql2");
+
+describe("Messages", () => {
+    const db = mysql.createPool({
+        connectionLimit: 2,
+    });
+
+    it("creates a message to the system", async () => {
+        expect(await db.query("")).toBe(true);
+    });
+
+    it.todo("distributes the message to the other servers");
+
+});
