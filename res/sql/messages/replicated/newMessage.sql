793667593c935d854e54623eeb667277eda939ab
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-09 03:03:33 +0800

    added query files

---
+++ res/sql/messages/replicated/newMessage.sql
@@ -0,0 +1,10 @@
+INSERT INTO origin_message (messageid, server_origin, author)
+VALUES (
+    ?,
+    (SELECT localid
+        FROM servers
+        WHERE serverid=?),
+    (SELECT localid
+        FROM users
+        WHERE localid=?)
+);
