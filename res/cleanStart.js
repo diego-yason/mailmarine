bc5672339ae434438498ffb75d647f4c017004ea
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-09 18:40:22 +0800

    updated scripts

--- res/cleanStart.js
+++ res/cleanStart.js
@@ -1,5 +1,7 @@
 import fs from "fs";
 import child_process from "child_process";
+import index from "@root/js/index.js";
 
 fs.rmSync("@root/build", { recursive: true, force: true });
 child_process.execSync(`cd res\n./build.sh\n`);
+index();
