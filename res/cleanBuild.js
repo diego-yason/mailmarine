b14871d2f029eff0f57d58b30057a85fdea2705a
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-10 17:06:14 +0800

    removed scripts, doesn't seem necessary anymore

--- res/cleanBuild.js
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-import child_process from "child_process";
-
-fs.rmSync("@root/build", { recursive: true, force: true });
-child_process.execSync(`cd res\n./build.sh\n`);
--- res/cleanStart.js
+++
@@ -1,7 +0,0 @@
-import fs from "fs";
-import child_process from "child_process";
-import index from "../js/test.js";
-
-fs.rmSync("@root/build", { recursive: true, force: true });
-child_process.exec(`./build.sh`);
-index();
