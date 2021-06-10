230febcd0b23c4ab81fe9b20923c6a88199e6783
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-11 03:21:18 +0800

    wrapped await

--- ts/util/server.ts
+++ ts/util/server.ts
@@ -1,6 +1,8 @@
 import * as channelcache from "memory-cache";
 import * as Db from "res/types/database";
 
+const db = globalThis.db;
+
 setInterval(() => {
     channelcache.clear();
 }, parseInt(process.env.CACHE) || 10800000);
@@ -10,7 +12,7 @@ export const getChannel = async (serverId: string): Promise<string> => {
         return new Promise((res) => res(channelcache.get(serverId)));
     }
 
-    const query: Db.Servers[] = await globalThis.db.execute(readSql("@sql/servers/getServer.sql"), [serverId])[0];
+    const query: Db.Servers[] = await (db.execute(readSql("@sql/servers/getServer.sql"), [serverId]))[0];
 
     return new Promise((res, rej) => {
         if (query.length != 1) {
