8bcd7339fb99fc2e2b96cffcf0d5c9dbf808b1ae
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-10 22:05:23 +0800

    updated process to check for user

--- ts/util/message.ts
+++ ts/util/message.ts
@@ -9,17 +9,22 @@ function check(err: unknown): void {
 
 export const createMessage = async (message: Message): Promise<void> => {
     // check if user exists in the db and isn't banned
-    const readUser = await db.execute(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check);
 
-    if (!readUser[0][0].id) {
+    const user = await db.execute(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check)[0];
+
+    if (!user.id) {
         // user isn't registered yet to the system
-        await db.query(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check);
+        db.query(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check);
+    } else {
+
     }
 
     db.execute(readfile("@sql/messages/origin/newMessage.sql"), [message.id, message.guild.id, message.author.id]).catch(check);
 };
 
 export const deleteMessage = async (messageId: string): Promise<void> => {
-    const origin = await db.execute(readfile("@sql/messages/origin/findMessage.sql"), [messageId]).catch(check);
-    const replicated = await db.execute(readfile("@sql/messages/findMessage.sql")).catch(check);
+    const origin = await db.execute(readfile("@sql/messages/origin/findMessage.sql"), [messageId]).catch(check)[0];
+    const replicated = await db.execute(readfile("@sql/messages/findMessage.sql")).catch(check)[0];
+
+    db.execute(readfile("@sql/messages/origin/deleteMessage.sql"), [origin.deleted]).catch(check);
 };
