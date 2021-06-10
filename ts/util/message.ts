3efc7745e290fc653449a3b62516e4981e19df3a
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-11 01:35:34 +0800

    added user check

--- ts/util/message.ts
+++ ts/util/message.ts
@@ -17,13 +17,16 @@ setInterval(() => {
 export const createMessage = async (message: Message): Promise<void> => {
     // check if user exists in the db and isn't banned
 
-    const user = await db.execute(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check)[0];
+    const user: Db.User = await db.execute(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check)[0][0];
 
-    if (!user.id) {
+    if (!user.localid) {
         // user isn't registered yet to the system
         db.query(readfile("@sql/users/readUsersById.sql"), [message.author.id]).catch(check);
     } else {
-
+        const banDetails: Db.Bans = await db.query(readfile("@sql/users/checkForBan.sql"), ["user", user.localid]).catch(check)[0][0];
+        if ((parseInt(banDetails.expiryDate) > new Date().getTime()) || !banDetails.temporary) {
+            return;
+        }
     }
 
     db.execute(readfile("@sql/messages/origin/newMessage.sql"), [message.id, message.guild.id, message.author.id]).catch(check);
