8ace43de9c45accab6449541731cd167757b090e
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-12 00:39:40 +0800

    boilerplate code

---
+++ res/messages/embed.ts
@@ -0,0 +1,8 @@
+import { Message, User } from "discord.js";
+import { Embed } from "../types/discord";
+
+export default function(user: User, message: Message, localId: number): Embed {
+    return {
+        
+    };
+}
