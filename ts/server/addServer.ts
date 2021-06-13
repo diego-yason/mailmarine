ba8b795ee819956cae92381dea0dbe138d25c49c
KingMarine-GH <61825575+KingMarine-GH@users.noreply.github.com>
2021-06-14 02:24:20 +0800

    too tired

--- ts/server/addServer.ts
+++ ts/server/addServer.ts
@@ -1,10 +1,30 @@
 import * as Db from "res/types/database";
 import { Interaction } from "res/types/interaction";
 
+/**
+ * Command ID of the /
+ */
+const addChannel = ""
+const removeChannel = ""
+
 export default (interaction: Interaction): void => {
     axios.post(`/interactions/${interaction.id}/${interaction.token}/callback`, {
         type: 5
     });
 
-    if (interaction.member.permissions)
+    // convert number to binary
+    const bit = parseInt(interaction.member.permissions).toString(2);
+
+    //  check if 6th bit is 1
+    if (bit[(bit.length - 1) - 5] == "1") {
+        // they have manage server permissions
+
+        // validate if its a normal channel
+        // TODO: this
+        if ()
+
+        axios.put(`/applications/${process.env.APPID}/guilds/${interaction.guild_id}/commands/${addChannel}/permissions`, {
+            
+        });
+    }
 }
